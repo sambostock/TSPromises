1acb6fb8d496680ea084299f00cc09f3207637ad
Kevin Rutkay <krutkay@gmail.com>
2019-02-20 21:07:34 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+function foo() {
+    console.log('foo()');
+    return new Promise(function (resolve, reject) {
+        console.log('Inside foo() promise');
+    });
+}
+console.log('Calling foo()');
+var p = foo();
+console.log('Done foo()');
+console.log(p);
+console.log('Then-ing promise');
+p.then(function () {
+    console.log('Promise complete');
+});
---
+++ index.ts
@@ -0,0 +1,18 @@
+function foo(): Promise<void> {
+	console.log('foo()');
+
+	return new Promise<void>((resolve: () => void, reject: () => void) => {
+		console.log('foo() promise');
+	});
+}
+
+console.log('Calling foo()');
+const p = foo();
+console.log('Done foo()');
+
+console.log(p);
+
+console.log('Then-ing promise')
+p.then(() => {
+	console.log('Promise complete');
+});
